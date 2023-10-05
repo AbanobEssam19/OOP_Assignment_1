962abfec48678b49f036947f06048eb3e9b30be7
Abanob Essam <abanobessam19@gmail.com>
2023-10-05 13:21:40 +0300

    Getting things ready

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+
+unsigned char image[SIZE][SIZE];
+
+void loadImage ();
+void saveImage ();
+
+int main()
+{
+    loadImage();
+    while (true) {
+        cout<<"Please select a filter to apply or 0 to exit: "<<'\n';
+        cout<<"1- Black & White Filter\n"
+              "2- Invert Filter\n"
+              "3- Merge Filter\n"
+              "4- Flip Image\n"
+              "5- Rotate Image\n"
+              "6- Darken and Lighten Image\n"
+              "7- Detect Image Edges \n"
+              "8- Enlarge Image\n"
+              "9- Shrink Image\n"
+              "a- Mirror 1/2 Image\n"
+              "b- Shuffle Image\n"
+              "c- Blur Image\n"
+              "d- Crop Image\n"
+              "e- Skew Image Right\n"
+              "f- Skew Image Up\n"
+              "s- Save the image to a file\n"
+              "0- Exit\n";
+        char c; cin>>c;
+        switch (c) {
+            case '0':
+                saveImage();
+                return 0;
+        }
+    }
+}
+
+void loadImage () {
+    char imageFileName[100];
+
+    cout << "Enter the source image file name: ";
+    cin >> imageFileName;
+
+    strcat (imageFileName, ".bmp");
+    readGSBMP(imageFileName, image);
+}
+
+void saveImage () {
+    char imageFileName[100];
+
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    strcat (imageFileName, ".bmp");
+    writeGSBMP(imageFileName, image);
+}
